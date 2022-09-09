9296a52be9df57c1ec0604695d0046b25c2c16bc
draymone <socotcho@gmail.com>
2022-09-09 11:25:59 +0200

    Remove commentarys in kube js code

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -1,11 +1,7 @@
-// priority: 0
-
 settings.logAddedRecipes = true
 settings.logRemovedRecipes = true
 settings.logSkippedRecipes = false
 settings.logErroringRecipes = true
-
-console.info('Hello, World! (You will see this line every time server resources reload)')
 
 onEvent('recipes', event => {
 	// Change recipes here
