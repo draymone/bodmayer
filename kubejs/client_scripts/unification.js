2280c3ea66661f4b7a9abb5365bf28dedb569ea7
draymone <socotcho@gmail.com>
2022-09-11 13:27:50 +0200

    Hide unified items in jei

---
+++ kubejs/client_scripts/unification.js
@@ -0,0 +1,8 @@
+onEvent('jei.hide.items', event => {
+	// event.hide('minecraft:cobblestone')
+    //Uranium
+    event.hide(['immersiveengineering:raw_block_uranium', 'immersiveengineering:storage_uranium', 'immersiveengineering:ingot_uranium', 'immersiveengineering:raw_uranium', 'immersiveengineering:dust_uranium', 'immersiveengineering:nugget_uranium', 'immersiveengineering:ore_uranium', 'immersiveengineering:deepslate_ore_uranium', 'immersiveengineering:slab_storage_uranium']);
+
+    //Lead
+    event.hide(['immersiveengineering:nugget_lead', 'immersiveengineering:ore_lead', 'immersiveengineering:deepslate_ore_lead', 'immersiveengineering:storage_lead', 'immersiveengineering:raw_block_lead', 'immersiveengineering:raw_lead', 'immersiveengineering:ingot_lead', 'immersiveengineering:plate_lead', 'immersiveengineering:dust_lead', 'mekanism:dirty_dust_lead', 'mekanism:shard_lead', 'mekanism:nugget_lead', 'mekanism:lead_ore', 'mekanism:deepslate_lead_ore', 'mekanism:block_lead', 'mekanism:block_raw_lead', 'mekanism:raw_lead', 'mekanism:ingot_lead', 'mekanism:crystal_lead', 'mekanism:clump_lead', 'mekanism:dust_lead', 'immersiveengineering:wire_lead'])
+})
